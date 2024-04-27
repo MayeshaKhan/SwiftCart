20987b093f3e16fee7eb7158eb7d0be11812d24a
20-43829-2 <mayeshakhan2k18@gmail.com>
2024-04-27 13:38:54 +0600

    swiftcart folder added

---
+++ SwiftCart/DAL/Repos/AgentRepo.cs
@@ -0,0 +1,49 @@
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class AgentRepo : Repo, IRepo<Agent, int, bool>
+    {
+        public void Create(Agent obj)
+        {
+            db.Agents.Add(obj);
+            db.SaveChanges();
+        }
+
+        
+        public bool Delete(int id)
+        {
+            var exobj = Get(id);
+            db.Agents.Remove(exobj);
+            return db.SaveChanges() > 0;
+        }
+
+        public Agent Get(int id)
+        {
+            return db.Agents.Find(id);
+        }
+
+        public List<Agent> Get()
+        {
+            return db.Agents.ToList();
+        }
+
+        public bool Update(Agent obj)
+        {
+            var exobj = Get(obj.Id);
+            db.Entry(exobj).CurrentValues.SetValues(obj);
+            return db.SaveChanges() > 0;
+        }
+
+        
+
+       
+    }
+}
+
---
+++ SwiftCart/SwiftCart/Controllers/AgentController.cs
@@ -0,0 +1,59 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SwiftCart.Controllers
+{
+    public class AgentController : ApiController
+    {
+        [HttpPost]
+        [Route("api/agent/create")]
+        public HttpResponseMessage Create(AgentDTO a)
+        {
+            AgentService.Create(a);
+            return Request.CreateResponse(HttpStatusCode.OK);
+
+        }
+        [HttpGet]
+        [Route("api/agent/{id}")]
+        public HttpResponseMessage Get(int id)
+        {
+            var data = AgentService.Get(id);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        [HttpGet]
+        [Route("api/agent/all")]
+        public HttpResponseMessage Get()
+        {
+            var data = AgentService.Get();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        [HttpPut]
+        [Route("api/agent/update")]
+        public HttpResponseMessage Update(AgentDTO a)
+        {
+            AgentService.Update(a);
+           
+                
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [HttpDelete]
+        [Route("api/agent/delete/{id}")]
+        public HttpResponseMessage Delete(int id)
+        {
+            AgentService.Delete(id);
+            
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+    }
+
+
+}
+
